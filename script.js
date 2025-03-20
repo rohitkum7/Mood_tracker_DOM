6704c3887832924abe7e7c744ddbdc06667a2ab2
rohit kumar <rohitkumar1215225@gmail.com>
2025-03-20 17:32:12 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,69 @@
+const today = new Date();
+let data = localStorage.getItem("data");
+let moodData = data ? JSON.parse(data) : [];
+let todayMood;
+
+//Todays date using moment
+const date = `${moment(new Date()).format("D/M/YYYY")}`;
+
+//Access The element
+document.querySelector("[data-currentDate]").textContent = date;
+const moodContainer = document.querySelector("[data-display-mood]");
+
+// getting all the moods as an array
+const moods = document.getElementById("mood").children;
+const moodsArray = [...moods];
+console.log(moodsArray);
+
+//Function to display the mood
+
+function displayMood(value, needDate) {
+  console.log(needDate);
+
+  if (!needDate) needDate = date;
+  const emoji = moodsArray.filter((mood) => {
+    if (mood.childNodes[1].alt === value) return mood;
+  });
+  let html = `<h1>I ${needDate === date ? "feel" : `felt`} <img src="${
+    emoji[0].childNodes[1].attributes[0].nodeValue
+  }" width="30px" height="30px"/> ${value} ${
+    needDate === date ? "today" : ` on ${needDate}`
+  }</h1>`;
+
+  moodContainer.insertAdjacentHTML("beforeend", html);
+}
+
+//Function to check today's mood
+
+const checkMoodSelected = function () {
+  for (let i = 0; i < moodData.length; i++) {
+    if (moodData[i].split("-")[1] === date) {
+      todayMood = moodData[i].split("-")[0];
+      return true;
+    }
+  }
+};
+
+const isMoodSelected = checkMoodSelected();
+
+//Condition that if you have seleced the todays mood again you cannot select
+if (isMoodSelected) {
+  moodData.forEach((data) => {
+    let date = data.split("-")[1];
+
+    let mood = data.split("-")[0];
+    displayMood(mood, date);
+  });
+} else {
+  moodsArray.forEach((mood) =>
+    mood.addEventListener("click", (e) => {
+      console.log(e.target.alt);
+      displayMood(e.target.alt);
+      let storageData = `${e.target.alt}-${date}`;
+      moodData.push(storageData);
+      console.log(moodData);
+
+      localStorage.setItem("data", JSON.stringify(moodData));
+    })
+  );
+}
